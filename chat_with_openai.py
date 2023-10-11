8c122138a73143f33ff63e88563e26dc3ca94908
Mehedi Nayem <naembfh@gmail.com>
2023-10-12 06:42:04 +0800

    try to reduce response time

--- chat_with_openai.py
+++ chat_with_openai.py
@@ -1,8 +1,15 @@
 import openai
 import json
+import time
 from apiKey import openAiApiKey
 
 openai.api_key = openAiApiKey
+
+# for smoothly show
+# def typeSmoothly(text, delay=0.03):
+#     for char in text:
+#         print(char, end="", flush=True)
+#         time.sleep(delay)
 
 def chat_with_gpt():
     print("Welcome to Chat Completion with OpenAi! Type 'exit' to end chat \n")
@@ -11,22 +18,28 @@ def chat_with_gpt():
         if userInput.lower() == "exit":
             print("Bye! Thanks for using Chat Completion with OpenAi")
             break
-        # response = openai.Completion.create(
-        #     engine="davinci",
-        #     prompt=f"You: {userInput}\n",
-        #     max_tokens=500
-        # )
+        
+        conversation = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": userInput}
+        ]
+        
+        startTime = time.time()
+        
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": userInput}
-            ]
+            messages = conversation
         )
-
-       
-        print("AI: " + response['choices'][0]['message']['content'])
-
+        
+        endTime = time.time()
+        responseTime = abs(startTime - endTime)
+        responseContent = response['choices'][0]['message']['content']
+        
+        print("AI is typing...\n", end="", flush=True)
+        print("\nAI: " + responseContent)
+        # typeSmoothly(responseContent)
+        print("\nResponse time: {:.2f} seconds".format(responseTime))
+        
         userChoice = input("Do you want to continue? type 'yes' to continue, any key else to exit:\n")
         if userChoice.lower() != "yes":
             print("Bye! Thanks for using chat completion with OpenAi")
