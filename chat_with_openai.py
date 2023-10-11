aea1c549ae49eed128a43587dcad68b7c972b8d3
Mehedi Nayem <naembfh@gmail.com>
2023-10-12 05:09:35 +0800

    gpt-3.5-turbo

--- chat_with_openai.py
+++ chat_with_openai.py
@@ -11,13 +11,22 @@ def chat_with_gpt():
         if userInput.lower() == "exit":
             print("Bye! Thanks for using Chat Completion with OpenAi")
             break
-        response = openai.Completion.create(
-            engine="davinci",
-            prompt=f"You: {userInput}\n",
-            max_tokens=500
+        # response = openai.Completion.create(
+        #     engine="davinci",
+        #     prompt=f"You: {userInput}\n",
+        #     max_tokens=500
+        # )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": userInput}
+            ]
         )
+
        
-        print("AI: " + response.choices[0].text)
+        print("AI: " + response['choices'][0]['message']['content'])
+
         userChoice = input("Do you want to continue? type 'yes' to continue, any key else to exit:\n")
         if userChoice.lower() != "yes":
             print("Bye! Thanks for using chat completion with OpenAi")
